773d163fdb4447d9e8e6924b48f65d55579a193a
Justin Le <lejustin.lv@gmail.com>
2016-10-12 15:20:05 -0700

    Test xgb

--- classify_pima.py
+++ classify_pima.py
@@ -5,6 +5,7 @@ from features_pima import *
 import xgboost as xgb
 from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import log_loss, roc_auc_score, f1_score, accuracy_score
 from sklearn.ensemble import RandomForestClassifier, \
                              ExtraTreesClassifier
 
@@ -39,17 +40,43 @@ print results
 print '\n'
 """
 
+clf = XGBClassifier(n_estimators=100, #70
+                    max_depth=3, 
+                    min_child_weight=5, 
+                    gamma=0.5, 
+                    learning_rate=0.05, 
+                    subsample=0.7, 
+                    colsample_bytree=0.7, 
+                    reg_alpha=0.001,
+                    seed=1)
+clf.fit(X_train, y_train, eval_metric='logloss')
+        
+pred = clf.predict(X_test)
+print "\nlog-loss of XGB: " 
+print log_loss(y_test, pred)
+print "\nAUC of XGB: " 
+print roc_auc_score(y_test, pred)
+print "\nF1-score of XGB: " 
+print f1_score(y_test, pred)
+print "\nAccuracy of XGB: " 
+print accuracy_score(y_test, pred)
+print "\n"
+
+"""
+# xgb_param = clf.get_xgb_params()
+# num_boost_round = clf.get_params()['n_estimators']
+# cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=num_boost_round, nfold=10)
+# print cvresult
+
 # Forest hyperparameters
 max_features = [4, 5]
-# n_estimators = [n_estimators]
+n_estimators = [n_estimators]
 
-models = [XGBClassifier(n_estimators=40, max_depth=3, min_child_weight=5, learning_rate=0.1, subsample=0.5, colsample_bytree=0.5)]
-params = [{'gamma': [i/10.0 for i in range(0, 5)]}]
 
-# models = [RandomForestClassifier(),
-#           ExtraTreesClassifier()]
+models = [RandomForestClassifier(),
+          ExtraTreesClassifier()]
 
-# params = [{'max_features': max_features, 'n_estimators': n_estimators}]
+params = [{'max_features': max_features, 'n_estimators': n_estimators}]
 
 for model in models:
     clf = GridSearchCV(model, param_grid=params, cv=10, scoring='neg_log_loss')
@@ -58,3 +85,4 @@ for model in models:
     print '\n'
     print results
     print '\n'
+"""
