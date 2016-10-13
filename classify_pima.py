815a78ee3055100bcba2c867248767e092488916
Justin Le <lejustin.lv@gmail.com>
2016-10-12 23:55:36 -0700

    Test forests across metrics

--- classify_pima.py
+++ classify_pima.py
@@ -13,32 +13,11 @@ data = pd.read_csv('./diabetes.csv')
 X = data.ix[:, :-1]
 y = data.ix[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=5)
 
 # XGBoost
 n_estimators = 100
 dtrain = xgb.DMatrix(X_train, y_train)
-
-"""
-params = {'bst:max_depth':2, 'bst:eta':1, 'silent':1, 'objective':'binary:logistic' }
-# bst = xgb.cv(params, dtrain, nfold=10)
-clf = xgb.XGBClassifier(max_depth = 5,
-                        n_estimators=n_estimators,
-                        learning_rate=0.1, 
-                        subsample=1.0,
-                        colsample_bytree=0.5,
-                        min_child_weight = 15,
-                        reg_alpha=0.03,
-                        seed=1)
-params = clf.get_xgb_params()
-cv = xgb.cv(params, dtrain, num_boost_round=n_estimators, nfold=10, seed=1)
-clf.fit(X_train, y_train, eval_metric='logloss',
-        eval_set=[(X_test, y_test)])
-results = pd.DataFrame(clf.evals_result())
-print '\n'
-print results
-print '\n'
-"""
 
 clf = XGBClassifier(n_estimators=100, #70
                     max_depth=3, 
@@ -49,6 +28,7 @@ clf = XGBClassifier(n_estimators=100, #70
                     colsample_bytree=0.7, 
                     reg_alpha=0.001,
                     seed=1)
+
 clf.fit(X_train, y_train, eval_metric='logloss')
         
 pred = clf.predict(X_test)
@@ -62,21 +42,48 @@ print "\nAccuracy of XGB: "
 print accuracy_score(y_test, pred)
 print "\n"
 
+clf = RandomForestClassifier(n_estimators=100, max_depth=4, max_features=5)
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print "\nlog-loss of Random Forest: " 
+print log_loss(y_test, pred)
+print "\nAUC of Random Forest: " 
+print roc_auc_score(y_test, pred)
+print "\nF1-score of Random Forest: " 
+print f1_score(y_test, pred)
+print "\nAccuracy of Random Forest: " 
+print accuracy_score(y_test, pred)
+print "\n"
+
+clf = ExtraTreesClassifier(n_estimators=100, max_depth=5, max_features=5)
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print "\nlog-loss of Extra Trees: " 
+print log_loss(y_test, pred)
+print "\nAUC of Extra Trees: " 
+print roc_auc_score(y_test, pred)
+print "\nF1-score of Extra Trees: " 
+print f1_score(y_test, pred)
+print "\nAccuracy of Extra Trees: " 
+print accuracy_score(y_test, pred)
+print "\n"
+
 """
-# xgb_param = clf.get_xgb_params()
-# num_boost_round = clf.get_params()['n_estimators']
-# cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=num_boost_round, nfold=10)
-# print cvresult
+# XGBoost CV
+xgb_param = clf.get_xgb_params()
+num_boost_round = clf.get_params()['n_estimators']
+cvresult = xgb.cv(xgb_param, dtrain, num_boost_round=num_boost_round, nfold=10, seed=1)
+print cvresult
 
-# Forest hyperparameters
-max_features = [4, 5]
+# Forest grid search
+max_depth = [3, 4, 5]
+max_features = [3, 4, 5]
 n_estimators = [n_estimators]
-
 
 models = [RandomForestClassifier(),
           ExtraTreesClassifier()]
 
-params = [{'max_features': max_features, 'n_estimators': n_estimators}]
+params = [{'max_depth': max_depth, 'max_features': max_features, 'n_estimators': n_estimators}]
 
 for model in models:
     clf = GridSearchCV(model, param_grid=params, cv=10, scoring='neg_log_loss')
